b11a07f7ea9bfb7a9ca1ee5d69450c17172e602b
Adrián Pino <adrian.pino@i2cat.net>
2025-05-06 15:10:05 +0200

    Fix helm installation

--- cni-installation/cilium/install-helm.sh
+++ cni-installation/cilium/install-helm.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # ref: https://helm.sh/docs/intro/install/
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
